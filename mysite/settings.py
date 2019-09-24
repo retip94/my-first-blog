bb66eaac3f50db7cfa7f570154555451a67c7d29
Piotr Piekielny <38405004+retip94@users.noreply.github.com>
2019-09-24 11:42:48 +0200

    nothing

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$^b_#&ln7sm@b_d37m$l3)j6y&bipyncu1zqoxz&6g#o*hf-&6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
